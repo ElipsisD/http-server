364014f94e2a90c8a2eb7402890aba7c99fa5e70
Dmitry Boyko <da-boyko@mail.ru>
2024-06-30 11:24:24 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,8 +3,16 @@ import socket
 
 
 def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    with socket.create_server(("localhost", 4221)) as server_socket:
+        while True:
+            conn, addr = server_socket.accept()
+            response = conn.recv(1024)
+            url_path = response.decode().split("\r\n")[0].split()[1]
+            if url_path == "/":
+                conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+            else:
+                conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+            conn.close()
 
 
 if __name__ == "__main__":
