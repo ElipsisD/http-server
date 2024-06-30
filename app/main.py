bf3fdfc9a80c77e10e0a9e77308644c3bb71703c
Dmitry Boyko <da-boyko@mail.ru>
2024-06-30 14:01:42 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import socket
 
 CRLF = "\r\n"
+SUCCESS_STATUS_LINE = "HTTP/1.1 200 OK"
 
 
 def main():
@@ -11,28 +12,39 @@ def main():
             status_line, *headers, response_body = response.decode().split(CRLF)
             method, url_path, protocol = status_line.split()
 
-            success_status_line = "HTTP/1.1 200 OK"
+            headers_dict = {res[0]: res[1] for j in headers[:-1] if (res := j.split(": "))}
 
-            if url_path == "/":
-                conn.sendall((success_status_line + CRLF + CRLF).encode())
-            elif url_path.startswith("/echo"):
-                url_string = url_path.split("echo/")[-1]
-                response_headers = {
-                    "Content-Type": "text/plain",
-                    "Content-Length": len(url_string),
-                }
-                response = CRLF.join(
-                    [
-                        success_status_line,
-                        CRLF.join([f"{k}: {v}" for k, v in response_headers.items()]) + CRLF,
-                        url_string
-                    ]
-                )
-                conn.sendall(response.encode())
-            else:
-                conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+            match url_path.split("/"):
+                case ["", ""]:
+                    conn.sendall((SUCCESS_STATUS_LINE + CRLF + CRLF).encode())
+                case ["", "echo", *_]:
+                    echo_string = url_path.split("echo/")[-1]
+                    prefix = get_response_prefix(echo_string)
+                    response = prefix + CRLF + echo_string
+                    conn.sendall(response.encode())
+                case ["", "user-agent", *_]:
+                    user_agent = headers_dict["User-Agent"]
+                    prefix = get_response_prefix(user_agent)
+                    response = prefix + CRLF + user_agent
+                    conn.sendall(response.encode())
+                case _:
+                    conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
             conn.close()
+
+
+def get_response_prefix(content: str) -> str:
+    response_headers = {
+        "Content-Type": "text/plain",
+        "Content-Length": len(content),
+    }
+    response_prefix = CRLF.join(
+        [
+            SUCCESS_STATUS_LINE,
+            CRLF.join([f"{k}: {v}" for k, v in response_headers.items()]) + CRLF,
+        ]
+    )
+    return response_prefix
 
 
 if __name__ == "__main__":
